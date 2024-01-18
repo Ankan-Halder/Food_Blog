4a87e98e1c816381a31106f811a79609f83278e7
Ankan Halder <ankanhalder4@gmail.com>
2024-01-18 13:52:02 +0530

    changes in create page of frontend

--- frontend/src/Components/Create.jsx
+++ frontend/src/Components/Create.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Create = () => {
-  return <div>Create Page</div>;
+  return (
+    <div className="container my-2">
+      <h3 className="text-center">Enter the Post Data</h3>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Name of the food</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Price</label>
+          <input type="number" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
